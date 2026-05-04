69e4bda1475f57b9f779b3a31e553f36cdfe4ac5
Shyr0v <axel.branquart31@gmail.com>
2026-05-04 10:43:56 +0200

    Update

--- sql/log.sql
+++ sql/log.sql
@@ -1,19 +1,3 @@
-DROP TRIGGER IF EXISTS franchise_create ON franchise;
-DROP TRIGGER IF EXISTS franchise_update ON franchise;
-DROP TRIGGER IF EXISTS franchise_delete ON franchise;
-
-DROP TRIGGER IF EXISTS cinema_create ON cinema;
-DROP TRIGGER IF EXISTS cinema_update ON cinema;
-DROP TRIGGER IF EXISTS cinema_delete ON cinema;
-
-DROP FUNCTION IF EXISTS trigger_franchise_create();
-DROP FUNCTION IF EXISTS trigger_franchise_update();
-DROP FUNCTION IF EXISTS trigger_franchise_delete();
-
-DROP FUNCTION IF EXISTS trigger_cinema_create();
-DROP FUNCTION IF EXISTS trigger_cinema_update();
-DROP FUNCTION IF EXISTS trigger_cinema_delete();
-
 CREATE TABLE IF NOT EXISTS log (
                                    id_log SERIAL PRIMARY KEY,
                                    table_name VARCHAR(50) NOT NULL,
